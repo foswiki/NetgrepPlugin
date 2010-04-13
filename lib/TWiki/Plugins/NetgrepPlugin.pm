0cb31e81e3f22d79aff045c6eee25a1fa068c311
BryanThale <BryanThale@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-04-13 02:33:45 +0000

    Item8899: Perltidy
    git-svn-id: http://svn.foswiki.org/trunk/NetgrepPlugin@7177 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/NetgrepPlugin.pm
+++ lib/TWiki/Plugins/NetgrepPlugin.pm
@@ -39,10 +39,10 @@ package TWiki::Plugins::NetgrepPlugin;
 
 # =========================
 use vars qw(
-	    $web $topic $user $installWeb $VERSION $RELEASE $debug
-	    $defaultRefresh $defaultFormat $defaultSize
-	    $perlDigestMD5Found $defaultFilter $defaultColor
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $debug
+  $defaultRefresh $defaultFormat $defaultSize
+  $perlDigestMD5Found $defaultFilter $defaultColor
+);
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -57,109 +57,124 @@ $RELEASE = 'Dakar';
 $perlDigestMD5Found = 0;
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        &TWiki::Func::writeWarning( "Version mismatch between NetgrepPlugin and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1 ) {
+        &TWiki::Func::writeWarning(
+            "Version mismatch between NetgrepPlugin and Plugins.pm");
         return 0;
     }
 
     # Get plugin preferences
-    $defaultRefresh  = &TWiki::Func::getPreferencesValue( "NETGREPPLUGIN_REFRESH" ) || 15;
-    $defaultFilter   = &TWiki::Func::getPreferencesValue( "NETGREPPLUGIN_FILTER" );
-    $defaultFormat   = &TWiki::Func::getPreferencesValue( "NETGREPPLUGIN_FORMAT" ) || "(+1+)";
-    $defaultSize     = &TWiki::Func::getPreferencesValue( "NETGREPPLUGIN_SIZE" ) || "100%";
-    $defaultColor    = &TWiki::Func::getPreferencesValue( "NETGREPPLUGIN_COLOR" ) || "black";
+    $defaultRefresh = &TWiki::Func::getPreferencesValue("NETGREPPLUGIN_REFRESH")
+      || 15;
+    $defaultFilter = &TWiki::Func::getPreferencesValue("NETGREPPLUGIN_FILTER");
+    $defaultFormat = &TWiki::Func::getPreferencesValue("NETGREPPLUGIN_FORMAT")
+      || "(+1+)";
+    $defaultSize = &TWiki::Func::getPreferencesValue("NETGREPPLUGIN_SIZE")
+      || "100%";
+    $defaultColor = &TWiki::Func::getPreferencesValue("NETGREPPLUGIN_COLOR")
+      || "black";
 
     # Get plugin debug flag
-    $debug = &TWiki::Func::getPreferencesFlag( "NETGREPPLUGIN_DEBUG" );
+    $debug = &TWiki::Func::getPreferencesFlag("NETGREPPLUGIN_DEBUG");
 
     # Plugin correctly initialized
-    &TWiki::Func::writeDebug( "- TWiki::Plugins::NetgrepPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
+    &TWiki::Func::writeDebug(
+        "- TWiki::Plugins::NetgrepPlugin::initPlugin( $web.$topic ) is OK")
+      if $debug;
     return 1;
 }
 
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
-    
-    &TWiki::Func::writeDebug( "- NetgrepPlugin::commonTagsHandler( $_[2].$_[1] )" ); 
+
+    &TWiki::Func::writeDebug(
+        "- NetgrepPlugin::commonTagsHandler( $_[2].$_[1] )");
     $_[0] =~ s/( *)%NETGREP{(.*?)}%/_handleNetgrepTag( $1, $2 )/geo;
 }
 
 # =========================
-sub _errorMsg
-{
-    my( $thePre, $theMsg ) = @_;
+sub _errorMsg {
+    my ( $thePre, $theMsg ) = @_;
 
     return "$thePre| *NETGREP Plugin Error* |\n"
-         . "$thePre| $installWeb.NetgrepPlugin: $theMsg |\n";
+      . "$thePre| $installWeb.NetgrepPlugin: $theMsg |\n";
 }
 
 # =========================
-sub _getUrl
-{
-    my( $theUrl, $theRefresh ) = @_;
+sub _getUrl {
+    my ( $theUrl, $theRefresh ) = @_;
 
-    my $cacheDir = "";
+    my $cacheDir  = "";
     my $cacheFile = "";
-    if( $theRefresh ) {
-        $cacheDir  = TWiki::Func::getPubDir() . '/' . $installWeb . '/NetgrepPlugin';
-        $cacheDir  =~ /(.*)/;  $cacheDir  = $1; # untaint (save because only internal variables)
-        $cacheFile = $cacheDir . '/_url-' . Digest::MD5::md5_hex( $theUrl );
-        $cacheFile =~ /(.*)/;  $cacheFile = $1; # untaint
-        if( ( -e $cacheFile ) && ( ( time() - (stat(_))[9] ) <= ( $theRefresh * 60 ) ) ) {
-            # return cached version if it exists and isn't too old. 1440 = 24h * 60min
-            return TWiki::Func::readFile( $cacheFile );
+    if ($theRefresh) {
+        $cacheDir =
+          TWiki::Func::getPubDir() . '/' . $installWeb . '/NetgrepPlugin';
+        $cacheDir =~ /(.*)/;
+        $cacheDir = $1;    # untaint (save because only internal variables)
+        $cacheFile = $cacheDir . '/_url-' . Digest::MD5::md5_hex($theUrl);
+        $cacheFile =~ /(.*)/;
+        $cacheFile = $1;    # untaint
+        if (   ( -e $cacheFile )
+            && ( ( time() - ( stat(_) )[9] ) <= ( $theRefresh * 60 ) ) )
+        {
+
+      # return cached version if it exists and isn't too old. 1440 = 24h * 60min
+            return TWiki::Func::readFile($cacheFile);
         }
     }
 
     my $host = "";
     my $port = 0;
     my $path = "";
-    if( $theUrl =~ /http\:\/\/(.*?)\:([0-9]+)(\/.*)/ ) {
+    if ( $theUrl =~ /http\:\/\/(.*?)\:([0-9]+)(\/.*)/ ) {
         $host = $1;
         $port = $2;
         $path = $3;
-    } elsif( $theUrl =~ /http\:\/\/(.*?)(\/.*)/ ) {
+    }
+    elsif ( $theUrl =~ /http\:\/\/(.*?)(\/.*)/ ) {
         $host = $1;
         $path = $2;
     }
-    unless( $path ) {
+    unless ($path) {
         return "ERROR: invalid format of the href parameter";
     }
-    # figure out how to get to TWiki::Net which is wide open in Cairo and before,
-    # but Dakar uses the session object.  
-    my $text = $TWiki::Plugins::SESSION->{net}
-	? $TWiki::Plugins::SESSION->{net}->getUrl( $host, $port, $path )
-	: TWiki::Net::getUrl( $host, $port, $path );
 
-    if( $text =~ /text\/plain\s*ERROR\: (.*)/s ) {
+   # figure out how to get to TWiki::Net which is wide open in Cairo and before,
+   # but Dakar uses the session object.
+    my $text =
+        $TWiki::Plugins::SESSION->{net}
+      ? $TWiki::Plugins::SESSION->{net}->getUrl( $host, $port, $path )
+      : TWiki::Net::getUrl( $host, $port, $path );
+
+    if ( $text =~ /text\/plain\s*ERROR\: (.*)/s ) {
         my $msg = $1;
         $msg =~ s/[\n\r]/ /gos;
         return "ERROR: Can't read $theUrl ($msg)";
     }
-    if( $text =~ /HTTP\/[0-9\.]+\s*([0-9]+)\s*([^\n]*)/s ) {
-        unless( $1 == 200 ) {
-           return "ERROR: Can't read $theUrl ($1 $2)";
+    if ( $text =~ /HTTP\/[0-9\.]+\s*([0-9]+)\s*([^\n]*)/s ) {
+        unless ( $1 == 200 ) {
+            return "ERROR: Can't read $theUrl ($1 $2)";
         }
     }
     $text =~ s/\r\n/\n/gos;
     $text =~ s/\r/\n/gos;
-    $text =~ s/^.*?\n\n(.*)/$1/os;  # strip header
-    $text =~ s/\n/ /gos;            # new line to space
+    $text =~ s/^.*?\n\n(.*)/$1/os;    # strip header
+    $text =~ s/\n/ /gos;              # new line to space
     $text =~ s/ +/ /gos;
 
-    if( $theRefresh ) {
-        unless( -e $cacheDir ) {
+    if ($theRefresh) {
+        unless ( -e $cacheDir ) {
+
             # create the cache directory in the pub dir of the NetgrepPlugin
-            umask( 002 );
+            umask(002);
             mkdir( $cacheDir, 0775 );
         }
+
         # save text in cache file before returning it
         TWiki::Func::saveFile( $cacheFile, $text );
     }
@@ -168,54 +183,54 @@ sub _getUrl
 }
 
 # =========================
-sub _handleNetgrepTag
-{
-    my( $thePre, $theArgs ) = @_;
+sub _handleNetgrepTag {
+    my ( $thePre, $theArgs ) = @_;
 
-    unless( $perlDigestMD5Found ) {
-	# lazy loading of Perl module
-	eval {
-	    $perlDigestMD5Found = require Digest::MD5;
-	}
+    unless ($perlDigestMD5Found) {
+
+        # lazy loading of Perl module
+        eval { $perlDigestMD5Found = require Digest::MD5; };
     }
-    unless( $perlDigestMD5Found ) {
-	return _errorMsg( $thePre, "ERROR: Cannot locate Perl module Digest::MD5" );
-    }
-
-    my $href    = TWiki::Func::extractNameValuePair( $theArgs, "href" ) 
-	|| TWiki::Func::extractNameValuePair( $theArgs );
-    my $filter  = TWiki::Func::extractNameValuePair( $theArgs, "filter" ) 
-	|| TWiki::Func::extractNameValuePair( $theArgs );
-    my $refresh = TWiki::Func::extractNameValuePair( $theArgs, "refresh" ) 
-	|| $defaultRefresh;
-    my $format  = TWiki::Func::extractNameValuePair( $theArgs, "format" ) 
-	|| $defaultFormat;
-    my $size    = TWiki::Func::extractNameValuePair( $theArgs, "size" ) 
-	|| $defaultSize;
-    my $color   = TWiki::Func::extractNameValuePair( $theArgs, "color" ) 
-	|| $defaultColor;
-
-    unless( $href ) {
-	return _errorMsg( $thePre, "href parameter (source) is missing" );
+    unless ($perlDigestMD5Found) {
+        return _errorMsg( $thePre,
+            "ERROR: Cannot locate Perl module Digest::MD5" );
     }
 
-    unless( $filter ) {
-	return _errorMsg( $thePre, "filter parameter is missing" );
+    my $href = TWiki::Func::extractNameValuePair( $theArgs, "href" )
+      || TWiki::Func::extractNameValuePair($theArgs);
+    my $filter = TWiki::Func::extractNameValuePair( $theArgs, "filter" )
+      || TWiki::Func::extractNameValuePair($theArgs);
+    my $refresh = TWiki::Func::extractNameValuePair( $theArgs, "refresh" )
+      || $defaultRefresh;
+    my $format = TWiki::Func::extractNameValuePair( $theArgs, "format" )
+      || $defaultFormat;
+    my $size = TWiki::Func::extractNameValuePair( $theArgs, "size" )
+      || $defaultSize;
+    my $color = TWiki::Func::extractNameValuePair( $theArgs, "color" )
+      || $defaultColor;
+
+    unless ($href) {
+        return _errorMsg( $thePre, "href parameter (source) is missing" );
     }
-    
+
+    unless ($filter) {
+        return _errorMsg( $thePre, "filter parameter is missing" );
+    }
+
     my $raw = _getUrl( $href, $refresh );
-    if( $raw =~ /^ERROR\: (.*)/s ) {
-         return _errorMsg( $thePre, $1 );
-     }
+    if ( $raw =~ /^ERROR\: (.*)/s ) {
+        return _errorMsg( $thePre, $1 );
+    }
 
     ( my @filtered ) = $raw =~ /$filter/;
 
     my $result = $format;
-    for (my $i=0; $i < scalar(@filtered); $i++) {
-    	$result =~ s/\+$i\+/$filtered[$i]/g;
+    for ( my $i = 0 ; $i < scalar(@filtered) ; $i++ ) {
+        $result =~ s/\+$i\+/$filtered[$i]/g;
     }
-    
-    return "$thePre <noautolink><span style='color: $color; font-size: $size;'>$result</span></noautolink>";
+
+    return
+"$thePre <noautolink><span style='color: $color; font-size: $size;'>$result</span></noautolink>";
 }
 
 # =========================
